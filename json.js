70b6512cecbecbcfca11ddd8dbd22a802eb521d1
Jesse Read <darthnerdus@me.com>
2013-07-25 12:46:50 -0400

    Flushed out JSON

--- json.js
+++ json.js
@@ -1,36 +1,200 @@
 // JSON
-var artistCollection = {
+var serverQuery = {
     "artists": {
         "Daft Punk": {
             "albums": {
-                "Random Access Memories": {
+                "Homework": {
+                    "releaseYear": 1997,
                     "songs": [
                         {
-                            "title": "Forever",
-                            "duration": 88,
+                            "title": "Daftendirekt",
+                            "duration": 164
                         },
                         {
-                            "title": "Forever 2",
-                            "duration": 999,
+                            "title": "WDPK 83.7 FM",
+                            "duration": 28
+                        },
+                        {
+                            "title": "Revolution 909",
+                            "duration": 326
+                        },
+                        {
+                            "title": "Da Funk",
+                            "duration": 329
+                        },
+                        {
+                            "title": "Fresh",
+                            "duration": 244
+                        },
+                        {
+                            "title": "Around the World",
+                            "duration": 429
+                        },
+                        {
+                            "title": "Rollin' & Scratchin'",
+                            "duration": 447
+                        },
+                        {
+                            "title": "Teachers",
+                            "duration": 173
+                        },
+                        {
+                            "title": "High Fidelity",
+                            "duration": 362
+                        },
+                        {
+                            "title": "Rock'n Roll",
+                            "duration": 453
+                        },
+                        {
+                            "title": "Oh Yeah",
+                            "duration": 121
+                        },
+                        {
+                            "title": "Burnin'",
+                            "duration": 413
+                        },
+                        {
+                            "title": "Indo Silver Club",
+                            "duration": 274
+                        },
+                        {
+                            "title": "Alive",
+                            "duration": 315
+                        },
+                        {
+                            "title": "Funk Ad",
+                            "duration": 51
                         }
                     ]
                 },
-                "Discovery": {
-                    "songs": {
-                        1: {
-                            "title": "Discovery",
-                            "duration": 512.
+                "Random Access Memories": {
+                    "releaseYear": 2013,
+                    "songs": [
+                        {
+                            "title": "Give Life Back to Music",
+                            "duration": 270,
                         },
-                        2: {
-                            "title": "The Music Sounds Better With You",
-                            "duration": 49,
+                        {
+                            "title": "The Game of Love",
+                            "duration": 321,
+                        },
+                        {
+                            "title": "Giorgio by Moroder",
+                            "duration": 543
+                        },
+                        {
+                            "title": "Within",
+                            "duration": 225
+                        },
+                        {
+                            "title": "Instant Crush",
+                            "duration": 333,
+                        },
+                        {
+                            "title": "Lose Yourself to Dance",
+                            "duration": 350,
+                        },
+                        {
+                            "title": "Touch",
+                            "duration": 497
+                        },
+                        {
+                            "title": "Get Lucky",
+                            "duration": 366
+                        },
+                        {
+                            "title": "Beyond",
+                            "duration": 288
+                        },
+                        {
+                            "title": "Motherboard",
+                            "duration": 340
+                        },
+                        {
+                            "title": "Fragments of Time",
+                            "duration": 277
+                        },
+                        {
+                            "title": "Contact",
+                            "duration": 379
                         }
-                    }
-                }
+                    ]
+                },
             },
         },
         "Pink Floyd": {
-            "albums": ["Random Access Memories", "Test"],
+            "albums": {
+                "The Dark Side of the Moon": {
+                    "releaseYear": 1973,
+                    "songs": [
+                        {
+                            "title": "Speak to Me",
+                            "duration": 67
+                        },
+                        {
+                            "title": "Breathe (In the Air)",
+                            "duration": 169
+                        },
+                        {
+                            "title": "On the Run",
+                            "duration": 225
+                        },
+                        {
+                            "title": "Time",
+                            "duration": 413
+                        },
+                        {
+                            "title": "The Great Gig in the Sky",
+                            "duration": 284
+                        },
+                        {
+                            "title": "Money",
+                            "duration": 383
+                        },
+                        {
+                            "title": "Us and Them",
+                            "duration": 469
+                        },
+                        {
+                            "title": "Any Colour You Like",
+                            "duration": 206
+                        },
+                        {
+                            "title": "Brain Damage",
+                            "duration": 226
+                        },
+                        {
+                            "title": "Eclipse",
+                            "duration": 132
+                        }
+                    ]
+                },
+                "Wish You Were Here" : {
+                    "releaseYear": 1975,
+                    "songs": [
+                        {
+                            "title": "Shine On You Crazy Diamond, Part One",
+                            "duration": 813
+                        },
+                        {
+                            "title": "Welcome to the Machine",
+                            "duration": 453
+                        },
+                        {
+                            "title": "Have a Cigar",
+                            "duration": 309
+                        },
+                        {
+                            "title": "Wish You Were Here",
+                            "duration": 336
+                        },
+                        {
+                            "title": "Shine On You Crazy Diamond, Part Two",
+                            "duration": 749
+                        }
+                    ]
+            }
         }
     }
 }
