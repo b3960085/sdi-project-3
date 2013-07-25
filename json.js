c68062f2f7516d26fa4649c410f21ebd5e0f5211
Jesse Read <darthnerdus@me.com>
2013-07-25 11:52:50 -0400

    Initial pass as JSON. Opting to use ordered array for song lists.

---
+++ json.js
@@ -0,0 +1,36 @@
+// JSON
+var artistCollection = {
+    "artists": {
+        "Daft Punk": {
+            "albums": {
+                "Random Access Memories": {
+                    "songs": [
+                        {
+                            "title": "Forever",
+                            "duration": 88,
+                        },
+                        {
+                            "title": "Forever 2",
+                            "duration": 999,
+                        }
+                    ]
+                },
+                "Discovery": {
+                    "songs": {
+                        1: {
+                            "title": "Discovery",
+                            "duration": 512.
+                        },
+                        2: {
+                            "title": "The Music Sounds Better With You",
+                            "duration": 49,
+                        }
+                    }
+                }
+            },
+        },
+        "Pink Floyd": {
+            "albums": ["Random Access Memories", "Test"],
+        }
+    }
+}
--- read_jesse_assignment3.js
+++ read_jesse_assignment3.js
@@ -1,1 +1,21 @@
-alert("JavaScript works!");
+/*  Jesse Read
+    Project 3
+    07/24/13
+    Theme: A music-playing application for a mobile device. */
+
+for (var key in artistCollection.artists) {
+    var artist = {
+        "name": key,
+        "albums": new Object(),
+    }
+    for (var albumKey in artistCollection.artists[key].albums) {
+        console.log(albumKey);
+        console.log(Object.keys(artistCollection.artists[key].albums[albumKey].songs).length);
+        artist.albums[albumKey] = artistCollection.artists[key].albums[albumKey];
+        //artist.albums[albumKey] = {
+        //    "Test": "test",
+        //    "Boogers": "boog",
+        //}
+    }
+    console.log(artist);
+}
