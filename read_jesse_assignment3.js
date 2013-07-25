9bc2ba12dabddd7f499b19e140ce5188789b22af
Jesse Read <darthnerdus@me.com>
2013-07-25 14:17:12 -0400

    Fixed function call, placed function definition before call.

--- read_jesse_assignment3.js
+++ read_jesse_assignment3.js
@@ -20,4 +20,37 @@ if (!remoteAvailable) {
     }
 }
 
-var artistCollection = function parseArtists (serverQuery);
+
+
+
+var parseArtists = function (serverQuery) {
+    for (var key in serverQuery.artists) {
+        var totalDuration = 0;
+        var artist = {
+            "name": key,
+            "albums":{},
+            "duration": 0,
+        }
+        for (var albumKey in serverQuery.artists[key].albums) {
+            var albumDuration = 0;
+            //console.log(albumKey);
+            //console.log(Object.keys(serverQuery.artists[key].albums[albumKey].songs).length);
+            artist.albums[albumKey] = serverQuery.artists[key].albums[albumKey];
+            artist.albums[albumKey].songs.forEach(function (song) {
+                albumDuration += song.duration;
+            });
+            artist.albums[albumKey].duration = albumDuration;
+            totalDuration += albumDuration
+            console.log(albumDuration);
+            //artist.albums[albumKey] = {
+            //    "Test": "test",
+            //    "Boogers": "boog",
+            //}
+        }
+        artist.duration = totalDuration;
+        console.log(totalDuration);
+        console.log(artist);
+    }
+}
+
+var artistCollection = parseArtists(serverQuery);
